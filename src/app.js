001edbd8151072fe1a022959299347db4b4548a0
Alex Younger <alexanderyounger@gmail.com>
2020-11-07 18:16:45 -0500

    fixed start script

--- src/app.js
+++ src/app.js
@@ -56,4 +56,6 @@ app.post("/", async (req, res, next) => {
 	}
 });
 
-app.listen(PORT, console.log("Server running on port " + PORT));
+app.listen(app.get("port"), () => {
+	console.log("Server running on port " + app.get("port"));
+});
